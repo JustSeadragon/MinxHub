520f0e06052e039d15b8d53e838b6dabec879d87
Sea 3 <82759997+JustSeadragon@users.noreply.github.com>
2023-02-26 15:51:19 +0000

    Update src.js

--- src.js
+++ src.js
@@ -6,7 +6,12 @@ alert(sm1);
 // Function to ask for API token
 function askAPI() {
   let token = prompt("API Token?");
-  console.log("%c", "padding: 50px; background:url(https://user-images.githubusercontent.com/82759997/221420603-e2de8ea7-0907-489a-a627-f32426a46251.png) no-repeat;");
+  const img = new Image();
+img.src = 'https://user-images.githubusercontent.com/82759997/221420603-e2de8ea7-0907-489a-a627-f32426a46251.png';
+
+img.onload = () => {
+  console.log('%c', `padding: ${img.height/2}px ${img.width/2}px; background:url(${img.src}) no-repeat;`);
+}
   if (token === "TWlueEh1YkFQSVRva2Vu") {
     alert("Logging in!");
   } else {
