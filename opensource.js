22d9270e8d8782d4f0a217add7c90cf73435570b
Sea 3 <82759997+JustSeadragon@users.noreply.github.com>
2023-03-01 20:31:31 +0000

    Update opensource.js

--- opensource.js
+++ opensource.js
@@ -1,4 +1,4 @@
-let responde fetch('https://raw.githubusercontent.com/JustSeadragon/MinxHub/main/src.js')
+let response fetch('https://raw.githubusercontent.com/JustSeadragon/MinxHub/main/src.js')
   .then(response => response())
   .then(code => eval()); 
   // This code can be used for any page.
